c9445c9070aeac40243ad1cb5a1e64b2fe7c95d3
PreYem <heavenly.onyx401@gmail.com>
2026-06-10 18:11:56 +0100

    added an end point on express that returns json data to confirm server is working correctly

--- express-backend/src/index.ts
+++ express-backend/src/index.ts
@@ -10,6 +10,12 @@ app.use(cors());
 app.use(express.json());
 app.use("/summarize", summarizeRouter);
 
+app.get("/", (request, response) => {
+  response.status(200).json({
+    successMessage: "Reddit Summarizer express backend is working",
+  });
+});
+
 app.listen(PORT, () => {
   console.log("Express bacckend running on http://localhost:" + PORT);
 });
